c65b6d9f023d00f421d5b313ae39ad90b5498533
Gopi Maguluri <66897720+Gopimaguluri267@users.noreply.github.com>
2021-05-22 15:16:54 +0530

    Update convertWAV.py

--- convertWAV.py
+++ convertWAV.py
@@ -5,5 +5,5 @@ from scipy.io.wavfile import write
 class convert():
     def to_wav(self, x):
         samples, sampling_rate = librosa.load(x)
-        scipy.io.wavfile.write('C:/Users/Gopi Maguluri/Raag Identification and Understanding/FRIU/static/uploads/new_sample.wav', sampling_rate, samples)
+        scipy.io.wavfile.write('/home/ubuntu/raag-identification/Raag_Identification/static/uploads/new_sample.wav', sampling_rate, samples)
         return None
